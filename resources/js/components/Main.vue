593794969e5530aae11c6d4e13b09c0350995669
rowl <airowl.gasga@gmail.com>
2022-05-27 15:54:05 +0200

    modificato pagina main per inserire router views

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -1,38 +1,14 @@
 <template>
     <main>
-        <div class="container">
-            <div class="row">
-                <Post v-for="(post, index) in posts" :key="index" :post="post"/>
-            </div>
-        </div>
+
     </main>
 </template>
 
 <script>
-import Post from './Post.vue';
+
 
 export default {
     name: 'Main',
-    components: {
-        Post
-    },
-    data: function(){
-        return {
-            posts: []
-        }
-    },
-    created() {
-        this.getPosts();
-    },
-    methods: {
-        getPosts(){
-            axios.get('http://localhost:8000/api/posts')
-            .then( (response) => {
-                console.log(response.data.data);
-                this.posts = response.data.data;
-            });
-        }
-    }
 }
 </script>
 
