29efcc10a3a7db9f28a0765730668aa5fd0f5bf0
rowl <airowl.gasga@gmail.com>
2022-05-27 15:53:50 +0200

    aggiunto componente header per inserire i router links

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">MANGA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Posts</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style>
+
+</style>
